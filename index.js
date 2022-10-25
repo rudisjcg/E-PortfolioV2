dc6dd11e5ee62d0fd8a9922a2dfba2db5f36dbc9
krakatua <rudisjcg@gmail.com>
2022-10-25 18:42:00 -0400

    some cool features with emailjs

---
+++ index.js
@@ -0,0 +1,24 @@
+//template_hrwqx1b
+//service_a6t5ufe
+//rW_c_LhJK_6vXIgrS
+
+function contact (event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+    emailjs.sendForm (
+            'service_a6t5ufe',
+            'template_hrwqx1b',
+            event.target,
+            'rW_c_LhJK_6vXIgrS'
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly on rudisjcg@gmail.com"
+            );
+        })
+}
