e09355d66b11c55bc6fa1bbe06646e93cee17aa8
krakatua <rudisjcg@gmail.com>
2022-10-27 16:22:34 -0400

    some js features

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-//template_hrwqx1b
-//service_a6t5ufe
-//rW_c_LhJK_6vXIgrS
+let isModalOpen = false;
+let contrastToggle = false;
+
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+
+    } else {
+        document.body.classList.remove("dark-theme")
+    }
+}
 
 function contact (event) {
     event.preventDefault();
@@ -23,7 +33,7 @@ function contact (event) {
         })
 }
 
-let isModalOpen = false;
+
 
 
 function toggleModal () {
